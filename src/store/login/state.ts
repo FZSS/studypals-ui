f69885198c198d3ccd7480c2f6ed20abfbab0c5f
Kevin Chen <kevin@fangzhou.io>
2021-05-17 21:07:12 -0400

    Reset login state

--- src/store/login/state.ts
+++ src/store/login/state.ts
@@ -5,7 +5,7 @@ export interface LoginState {
 }
 
 export const initialState: LoginState = {
-  loggedIn: true, // TODO: reset
+  loggedIn: false, // TODO: reset
   studentId: '12345',
   lectureId: '7',
 };
